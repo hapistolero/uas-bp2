564e72f2985ae2390e2c447b5333aadea5e5532f
Rizukaf-id <68379809+Rizukaf-id@users.noreply.github.com>
2022-06-14 12:50:19 +0700

    Upload Project -TEST2

---
+++ src/projectuas/test.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectuas;
+
+/**
+ *
+ * @author rizka
+ */
+public class test {
+    
+}
